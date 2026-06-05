ae3cd72f2606214587c745cbb4cfea6461804d5d
Cristian E. <encristian23@yahoo.com>
2026-06-05 14:20:48 +0300

    Create initial API testing project

---
+++ UsersTests.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace Reqres_API_Testing_CSharp;
+
+public class UsersTests
+{
+    [Test]
+    public async Task GetUsers_ShouldReturnSuccessStatusCode()
+    {
+        var client = new RestClient("https://jsonplaceholder.typicode.com");
+
+        var request = new RestRequest("/users", Method.Get);
+
+        var response = await client.ExecuteAsync(request);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+}
