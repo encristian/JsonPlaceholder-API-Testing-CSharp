9320b8cd23ca1e80a7a0e3bed0ac20415a77f0d6
Cristian E. <encristian23@yahoo.com>
2026-06-07 10:03:13 +0300

    Add GET users API tests

--- UsersTests.cs
+++ UsersTests.cs
@@ -1,5 +1,6 @@
 ﻿using RestSharp;
 using System.Net;
+using System.Text.Json;
 
 namespace Reqres_API_Testing_CSharp;
 
@@ -8,12 +9,48 @@ public class UsersTests
     [Test]
     public async Task GetUsers_ShouldReturnSuccessStatusCode()
     {
-        var client = new RestClient("https://jsonplaceholder.typicode.com");
+        using var client = new RestClient("https://jsonplaceholder.typicode.com");
 
         var request = new RestRequest("/users", Method.Get);
 
         var response = await client.ExecuteAsync(request);
 
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(response.Content, Is.Not.Null.And.Not.Empty);
+    }
+
+    [Test]
+    public async Task GetSingleUser_ShouldReturnUserDetails()
+    {
+        using var client = new RestClient("https://jsonplaceholder.typicode.com");
+
+        var request = new RestRequest("/users/1", Method.Get);
+
+        var response = await client.ExecuteAsync(request);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(response.Content, Is.Not.Null.And.Not.Empty);
+
+        using JsonDocument json = JsonDocument.Parse(response.Content!);
+
+        string name = json.RootElement.GetProperty("name").GetString()!;
+        string username = json.RootElement.GetProperty("username").GetString()!;
+        string email = json.RootElement.GetProperty("email").GetString()!;
+
+        Assert.That(name, Is.EqualTo("Leanne Graham"));
+        Assert.That(username, Is.EqualTo("Bret"));
+        Assert.That(email, Is.EqualTo("Sincere@april.biz"));
+    }
+
+    [Test]
+    public async Task GetSingleUser_WithInvalidId_ShouldReturnNotFound()
+    {
+        using var client = new RestClient("https://jsonplaceholder.typicode.com");
+
+        var request = new RestRequest("/users/999", Method.Get);
+
+        var response = await client.ExecuteAsync(request);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
     }
 }
