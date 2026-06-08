faa57e56056fcc7ac8fcbb6e421efaa74c7e1dba
Cristian E. <encristian23@yahoo.com>
2026-06-08 10:11:55 +0300

    Add delete user API test

---
+++ DeleteUserTests.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace Reqres_API_Testing_CSharp;
+
+public class DeleteUserTests
+{
+    [Test]
+    public async Task DeleteUser_ShouldReturnSuccessStatusCode()
+    {
+        using var client = new RestClient("https://jsonplaceholder.typicode.com");
+
+        var request = new RestRequest("/users/1", Method.Delete);
+
+        var response = await client.ExecuteAsync(request);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+}
