f76e2f227d8d2b6b7c9d85f76e215afc38ebbf91
Cristian E. <encristian23@yahoo.com>
2026-06-15 15:17:58 +0300

    Add negative API tests

--- Constants/ApiEndpoints.cs
+++ Constants/ApiEndpoints.cs
@@ -4,7 +4,14 @@ public static class ApiEndpoints
 {
     public const string Users = "/users";
 
+    public const string InvalidEndpoint = "/invalid-endpoint";
+
     public static string UserById(int id)
+    {
+        return $"/users/{id}";
+    }
+
+    public static string UserById(string id)
     {
         return $"/users/{id}";
     }
---
+++ NegativeUserTests.cs
@@ -0,0 +1,25 @@
+﻿using Reqres_API_Testing_CSharp.Base;
+using Reqres_API_Testing_CSharp.Constants;
+using Reqres_API_Testing_CSharp.Helpers;
+using System.Net;
+
+namespace Reqres_API_Testing_CSharp;
+
+public class NegativeUserTests : ApiTestBase
+{
+    [Test]
+    public async Task GetInvalidEndpoint_ShouldReturnNotFound()
+    {
+        var response = await GetAsync(ApiEndpoints.InvalidEndpoint);
+
+        ResponseAssertions.AssertStatusCode(response, HttpStatusCode.NotFound);
+    }
+
+    [Test]
+    public async Task GetUser_WithInvalidIdFormat_ShouldReturnNotFound()
+    {
+        var response = await GetAsync(ApiEndpoints.UserById("abc"));
+
+        ResponseAssertions.AssertStatusCode(response, HttpStatusCode.NotFound);
+    }
+}
